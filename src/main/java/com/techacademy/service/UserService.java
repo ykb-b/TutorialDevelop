c9032fcb9c2ff7819dfde5b6e6919deb826f4a78
ykb-b <lkpkoml14@gmail.com>
2025-04-01 21:25:49 +0900

    chapter7コミット

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -3,6 +3,7 @@ package com.techacademy.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
@@ -20,4 +21,13 @@ public class UserService {
         // リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
     }
+
+    // ----- 追加:ここから -----
+    /** Userの登録を行なう */
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+    // ----- 追加:ここまで -----
+
 }
