b7962513038021bfa5db7da87a49c0ddda91d43e
ykb-b <lkpkoml14@gmail.com>
2025-04-09 15:06:48 +0900

    Lesson18課題作成

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -59,15 +59,22 @@ public class UserController {
     /** User更新画面を表示 */
     @GetMapping("/update/{id}/")
     public String getUser(@PathVariable("id") Integer id, Model model) {
-        // Modelに登録
-        model.addAttribute("user", service.getUser(id));
-        // User更新画面に遷移
+        if (id != null) {
+            // Modelに登録
+            model.addAttribute("user", service.getUser(id));
+            // User更新画面に遷移
+        }
         return "user/update";
     }
 
     /** User更新処理 */
     @PostMapping("/update/{id}/")
-    public String postUser(User user) {
+    public String postUser(@Validated User user, BindingResult res, Model model) {
+        if(res.hasErrors()) {
+            Integer id = null;
+            model.addAttribute("user", user);
+            return getUser(id, model);
+        }
         // User登録
         service.saveUser(user);
         // 一覧画面にリダイレクト
